4cc459c79c336e7a0c8d0c9f9c64020dd609b3fb
jisu1997 <gyeong1997@naver.com>
2023-06-05 09:47:36 +0900

    blog

---
+++ chap01-connection-config/src/com/greedy/section02/xmlconfig/Application01.java
@@ -0,0 +1,10 @@
+package com.greedy.section02.xmlconfig;
+
+public class Application01 {
+
+	public static void main(String[] args) {
+		String resource = "com/greedy/section02/xmlconfig/mybatis-config.xml";
+
+	}
+
+}
